f833fcaf1f65866a7b80854f2999576a7e19f5a1
s115345 <s115345@ap.be>
2021-03-03 21:25:12 +0100

    verbetering

--- exercise01.py
+++ exercise01.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import os
 path_components = os.getcwd()
-resultaat = path_components.split("/")
-resultaat.remove('')
-print resultaat
+resultaat = path_components.strip(os.sep)
+resultaat = path_components.split(os.sep)
+while resultaat[0] == '':
+    resultaat.remove('')
+print (resultaat)
