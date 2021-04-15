312c74223b9b6e17c2843838c785e8c09b7815c4
s115345 <s115345@ap.be>
2021-04-15 23:45:51 +0200

    fwblock.py

---
+++ fwblock.py
@@ -0,0 +1,27 @@
+'''
+Functions to add rules to the firewall.
+'''
+
+import ipaddress
+
+blocked = set()
+
+def block_ip(ip):
+    '''
+    This function adds a rule to the firewall to block all traffic from
+    the given IP address.
+    ip: IP address to block traffic from.
+    '''
+    global blocked
+
+    ipaddress.ip_address(ip) # this will fail if an invalid IP address is passed
+
+    if ip in blocked:
+        raise RuntimeError('You\'re making the firewall slow! IP Address %s '
+                'has already been blocked' % ip)
+
+    blocked.add(ip)
+    print(blocked)
+  
+    block_ip(ip)
+    
