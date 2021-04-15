a310faa15c28b8752f6d71436b8b26e772a21f88
s115345 <s115345@ap.be>
2021-04-15 23:34:31 +0200

    ip-adressaanpassen

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,36 +1,47 @@
-import os
-
+import fwblock
 
 with open('sshdlog', 'r') as f:
     f_contents = f.read()
     print(f_contents)
+   
         
     import ipaddress
-
-    def block_ip(ip):
-        '''
-        This function adds a rule to the firewall to block all traffic from
-        the given IP address.
-        ip: IP address to block traffic from.
-        '''
-        global blocked
-        
-        try:
-
-
-            ipaddress.ip_address(ip) # this will fail if an invalid IP address is passed
-        except ValueError:
-                validip = False
-        if validip:
-                ipString = str(ip) + ''
+    
+    fwblock.block_ip('51.140.228.73')
+    
+#     def block_ip(ip):
+#         '''
+#         This function adds a rule to the firewall to block all traffic from
+#         the given IP address.
+#         ip: IP address to block traffic from.
+#         '''
+#         global blocked
+#         
+#         try:
+# 
+# 
+#             ipaddress.ip_address(ip) # this will fail if an invalid IP address is passed
+#         except ValueError:
+#                 validip = False
+#         if validip:
+#                 ipString = str(ip) + ''
+#                 
+#         if '142.93.141.126' in ipString:
+#             blocked =blocked.add(ip)   
+# 
+#         if ip in blocked:
+#                     raise RuntimeError('You\'re making the firewall slow! IP Address %s '
+#                     'has already been blocked' % ip)
+#                 
+#         def print_blocked():
+#             print (blocked)
+#             f.write("test")
+#             f.close()
+            
+            
                 
-        if '142.93.141.126' in ipString:
-                blocked.add(ip)   
-
-        if ip in blocked:
-                    raise RuntimeError('You\'re making the firewall slow! IP Address %s '
-                    'has already been blocked' % ip)
-        print (blocked)
+                
+         
 
             
 
