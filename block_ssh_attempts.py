64565af5bebc5c9e35a1cde9ec6a38196d950d2f
s115345 <s115345@ap.be>
2021-04-15 19:19:22 +0200

    poging-FunctieMaken

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -4,21 +4,33 @@ import os
 with open('sshdlog', 'r') as f:
     f_contents = f.read()
     print(f_contents)
-    
-import ipaddress
+        
+    import ipaddress
 
-def block_ip(ip):
-    '''
-    This function adds a rule to the firewall to block all traffic from
-    the given IP address.
-    ip: IP address to block traffic from.
-    '''
-    global blocked
+    def block_ip(ip):
+        '''
+        This function adds a rule to the firewall to block all traffic from
+        the given IP address.
+        ip: IP address to block traffic from.
+        '''
+        global blocked
+        
+        try:
 
-    ipaddress.ip_address(ip) # this will fail if an invalid IP address is passed
 
-    if ip in blocked:
-        raise RuntimeError('You\'re making the firewall slow! IP Address %s '
-                'has already been blocked' % ip)
+            ipaddress.ip_address(ip) # this will fail if an invalid IP address is passed
+        except ValueError:
+                validip = False
+        if validip:
+                ipString = str(ip) + ''
+                
+        if '142.93.141.126' in ipString:
+                blocked.add(ip)   
 
-    blocked.add(ip)
+        if ip in blocked:
+                    raise RuntimeError('You\'re making the firewall slow! IP Address %s '
+                    'has already been blocked' % ip)
+        print (blocked)
+
+            
+
