437e4ae8347a41aa7624002b9d52850706c50139
s115345 <s115345@ap.be>
2021-04-24 18:44:46 +0200

    Script-print-niets-af

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -11,14 +11,28 @@ def readLinesWithInvalid():
     SshOutput = list((""))
     for InvalidLines in f_contents():  #voor elke lijn in de logfile
         if "invalid user" in InvalidLines: SshOutput.append(InvalidLines) #als invalid user word gevonden in de lijn => in SshOutput list insteken
+        return SshOutput
 
 def readInvalidIpAdresses():
    
     InvalidUser = list((""))
-    for lines in f_contents: #lines doorlopen in logfile
+    for lines in SshOutput: #lines doorlopen in variable SshOutput => Invalid Lines
         split_elements=lines.split("")#splitten in elementen
         ip=split_elements[-4]#van "invalid" element naar ip element gaan
         InvalidUser.append(ip)#Ip element toevoegen aan InvalidUser list
+        return InvalidUser
+        
+def InvalidIpCount():
+
+    Counter =list((""))#list aanmaken voor ip te tellen
+    Counter = SshOutput.count("Invalid")#element invalid vinden in SshOutput via .count
+    if Counter == 3: 
+        fwblock.block_ip(InvalidUser)#ip blokkeren
+        print(InvalidUser + " Ip has been found 3 times or more, Ip has been blocked")
+        
+                
+        
+
 
     
     
