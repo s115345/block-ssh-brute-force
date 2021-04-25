1ce633dc93ea64b9d32a017b42afe03fcc44f49d
s115345 <s115345@ap.be>
2021-04-25 17:01:33 +0200

    Probeert-met-Dict-op-te-lossen-maar-is-niet-gelukt

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,42 +1,38 @@
 import fwblock
 
-def readLogfiles():
-    
-    with open('sshdlog', 'r') as f:
-        f_contents = f.read()
-        print(f_contents)
-        return f_contents
-# als je functie wilt sluiten moet je minder whitespace gebruiken
-def readLinesWithInvalid():
-    SshOutput = list((""))
-    for InvalidLines in f_contents():  #voor elke lijn in de logfile
-        if "invalid user" in InvalidLines: SshOutput.append(InvalidLines) #als invalid user word gevonden in de lijn => in SshOutput list insteken
-        return SshOutput
+SshOutPut = []
 
-def readInvalidIpAdresses():
-   
-    InvalidUser = list((""))
-    for lines in SshOutput: #lines doorlopen in variable SshOutput => Invalid Lines
-        split_elements=lines.split("")#splitten in elementen
-        ip=split_elements[-4]#van "invalid" element naar ip element gaan
-        InvalidUser.append(ip)#Ip element toevoegen aan InvalidUser list
-        return InvalidUser
+
+IpAdress =[]
+
+    
+with open('sshdlog', 'r') as f:
+    f_contents =f.read()
+for line in f_contents:
+    if "invalid user" in line: SshOutPut.append(line)
+    
         
-def InvalidIpCount():
+for line in SshOutPut:
+    listLines=line.split(" ")
+    ip=listLines[-4]
+    IpAdress.append(ip)
 
-    Counter =list((""))#list aanmaken voor ip te tellen
-    Counter = SshOutput.count("Invalid")#element invalid vinden in SshOutput via .count
-    if Counter == 3: 
-        fwblock.block_ip(InvalidUser)#ip blokkeren
-        print(InvalidUser + " Ip has been found 3 times or more, Ip has been blocked")
+
+IpChecker=set(IpAdress)
+
+for NieuweIp in IpChecker:
+    counter=0
+    for Iplijst in IpAdress:
+        if NieuweIp==Iplijst:
+            counter+=1
+
+    if counter>=3:
+      raise RuntimeError('The Ip has been dedected 3 times already' % ip)
+    fwblock.block_ip(a)       
         
-readLogfiles()          
-readLinesWithInvalid()
-readInvalidIpAdresses()
-InvalidIpCount()
+            
 
+fwblock.block_ip('45.85.90.17')
 
-    
-    
 
     
